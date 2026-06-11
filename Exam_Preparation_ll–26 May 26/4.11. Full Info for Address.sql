5c47af911fca86193d19e5797034d2c4b5d7bfdf
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-06-11 19:32:52 +0700

    the required solution

--- Exam_Preparation_ll–26 May 26/4.11. Full Info for Address.sql
+++ Exam_Preparation_ll–26 May 26/4.11. Full Info for Address.sql
@@ -13,38 +13,39 @@ CREATE OR REPLACE PROCEDURE sp_courses_by_address(address_name VARCHAR(100))
 AS
 $$
 BEGIN
-	TRUNCATE TABLE search_results RESTART IDENTITY;
-	
+	TRUNCATE TABLE search_results;
+
 	INSERT INTO search_results (
-		address_name,
-		full_name,
-		level_of_bill,
-		make,
-		"condition",
-		category_name
+    	address_name,
+    	full_name,
+    	level_of_bill,
+    	make,
+    	condition,
+    	category_name
 	)
 	SELECT
-		a.name,
+		a.name address_name,
 		cl.full_name,
 		CASE
-			WHEN cour.bill <= 20 THEN 'Low'
-			WHEN cour.bill <= 30 THEN 'Medium'
+			WHEN co.bill <= 20 THEN 'Low'
+			WHEN co.bill <= 30 THEN 'Medium'
 			ELSE 'High'
 		END AS level_of_bill,
 		c.make,
 		c.condition,
-		cat.name AS category_name
-	FROM addresses AS a
-	JOIN courses AS cour
-		ON a.id = cour.from_address_id
-	JOIN clients AS cl
-		ON cl.id = cour.client_id
-	JOIN cars AS c
-		ON c.id = cour.car_id
-	JOIN categories AS cat
-		ON cat.id = c.category_id
+		cat.name category_name
+	FROM addresses a
+	JOIN courses co
+		ON co.from_address_id = a.id
+	JOIN cars c
+		ON co.car_id = c.id
+	JOIN clients cl
+		ON co.client_id = cl.id
+	JOIN categories cat
+		ON c.category_id = cat.id
 	WHERE a.name = address_name
-	ORDER BY c.make, cl.full_name;
+	ORDER BY c.make, cl.full_name
+	;
 END;
 $$
 LANGUAGE plpgsql;
