dab5959406bd28c1b5fd3a40fc8ac2ceffa5f69e
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-06-12 14:06:34 +0700

    second version - not so pretty one

--- PostgreSQL Exam - 14 October 2023/4.10. Player Team Finder.sql
+++ PostgreSQL Exam - 14 October 2023/4.10. Player Team Finder.sql
@@ -16,3 +16,20 @@ END;
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE PROCEDURE sp_players_team_name(player_name VARCHAR(50), INOUT team_name VARCHAR(45))
+AS
+$$
+BEGIN
+	SELECT
+		CASE
+			WHEN t.name IS NULL THEN 'The player currently has no team'
+			ELSE t.name
+		END
+	INTO team_name
+	FROM players pl
+	LEFT JOIN teams t
+		ON pl.team_id = t.id
+	WHERE CONCAT_WS(' ', pl.first_name, pl.last_name) = player_name;
+END;
+$$
+LANGUAGE plpgsql;
