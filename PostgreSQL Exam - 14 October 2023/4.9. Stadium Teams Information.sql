31b71dc88f1c203c1892ba0ff420e1ecf5eef210
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-06-12 13:49:29 +0700

    second version

--- PostgreSQL Exam - 14 October 2023/4.9. Stadium Teams Information.sql
+++ PostgreSQL Exam - 14 October 2023/4.9. Stadium Teams Information.sql
@@ -14,3 +14,22 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+-- или
+
+CREATE OR REPLACE FUNCTION fn_stadium_team_name(stadium_name VARCHAR(30))
+RETURNS TABLE(team_names VARCHAR(45)) AS
+$$
+BEGIN
+	RETURN QUERY
+		SELECT
+			t.name
+		FROM stadiums st
+		JOIN teams t
+			ON st.id = t.stadium_id
+		WHERE st.name = stadium_name
+		ORDER BY t.name
+	;
+END;
+$$
+LANGUAGE plpgsql;
